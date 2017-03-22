9a9d1e7da8bd1894d8131ebbeea3ed72ce9717b6
wangjianan <wangjiananwork@163.com>
2017-03-22 22:43:02 +0800

    修复数据库域名指向

--- src/app/http/http.service.ts
+++ src/app/http/http.service.ts
@@ -4,7 +4,7 @@ import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HttpService {
-    private path = 'http://127.0.0.1:8080/jasmine/';
+    private path = 'http://api.oopmind.com/';
     private headers = new Headers(
         {
             'Content-Type': 'application/json; charset=utf-8'
